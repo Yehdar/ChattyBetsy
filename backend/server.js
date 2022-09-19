ae46ddca3b8cbc040ff602fa1952ecee72227f0f
Yehdar <108688044+Yehdar@users.noreply.github.com>
2022-09-19 16:08:43 -0400

    ChattyBetsy Mark 1
    "node_modules" file not added because it is too big

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+app.use(express.json()); // to accept JSON Data
+
+app.get("/", (req, res) => {
+  res.send("API is Running Successfully");
+});
+
+app.use("/api/user", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server Started on PORT ${PORT}`));
---
+++ src/Pages/Homepage.js
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+
+const Homepage = () => {
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        textAlign="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="1g"
+        borderWidth="1px"
+      >
+        <Text fontSize="5xl" fontFamily="Ubuntu" color="purple">
+          ChattyBetsy
+        </Text>
+      </Box>
+      <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="1g"
+        color="black"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded" colorScheme="pink">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default Homepage;
